843035809c1826123c08938152cf1abca4ba05a6
samuelvisser <samuvisser@gmail.com>
2026-09-01 17:53:03 +0200

    Add podcast episode-count download limit

--- server/backend/src/backend/db/models/download_profile/PodcastDownloadProfile.py
+++ server/backend/src/backend/db/models/download_profile/PodcastDownloadProfile.py
@@ -13,7 +13,8 @@ class PodcastDownloadProfile(DownloadProfileBase):
     download_with_countdown: Mapped[bool] = mapped_column(default=False)
     redownload_final: Mapped[bool] = mapped_column(default=False)
     download_days_in_past: Mapped[int] = mapped_column(default=0)
+    download_episode_count: Mapped[int] = mapped_column(default=0)
     delete_older_episodes: Mapped[bool] = mapped_column(default=True)
 
     def __repr__(self) -> str:
-        return f"<PodcastDownloadProfile(id={self.id}, show_id={self.show_id}, enable_profile={self.enable_profile}, download_days_in_past={self.download_days_in_past}, delete_older_episodes={self.delete_older_episodes}, created_at={self.created_at}, updated_at={self.updated_at})>"
+        return f"<PodcastDownloadProfile(id={self.id}, show_id={self.show_id}, enable_profile={self.enable_profile}, download_days_in_past={self.download_days_in_past}, download_episode_count={self.download_episode_count}, delete_older_episodes={self.delete_older_episodes}, created_at={self.created_at}, updated_at={self.updated_at})>"
