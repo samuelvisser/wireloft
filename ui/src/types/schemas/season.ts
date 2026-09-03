cb91965fe8d6fffce30db59670469e6baf10ace3
samuelvisser <samuvisser@gmail.com>
2026-09-04 00:36:07 +0200

    Fix local season frontend schema

--- ui/src/types/schemas/season.ts
+++ ui/src/types/schemas/season.ts
@@ -7,8 +7,8 @@ const SeasonBaseSchema = z.object({
 })
 
 export const SeasonCreateSchema = SeasonBaseSchema.extend({
-    dwId: z.string().min(1, "dwId missing"),
     showId: z.int().min(1, "showId missing"),
+    index: z.int(),
     slug: z.string().min(1, "slug missing"),
 })
 export type SeasonCreateIn = z.input<typeof SeasonCreateSchema>;
@@ -18,12 +18,14 @@ export type SeasonCreateOut = z.output<typeof SeasonCreateSchema>;
 // Schema for seasons without external relations, allowing for dynamic insertion
 export const SeasonDetachedSchema = SeasonCreateSchema.omit({
     showId: true,
+    index: true,
 })
 export type SeasonDetachedIn = z.input<typeof SeasonDetachedSchema>;
 export type SeasonDetachedOut = z.output<typeof SeasonDetachedSchema>;
 
 
 export const SeasonUpdateSchema = SeasonBaseSchema.extend({
+    index: z.int(),
 })
 export type SeasonUpdateIn = z.input<typeof SeasonUpdateSchema>;
 export type SeasonUpdateOut = z.output<typeof SeasonUpdateSchema>;
@@ -32,9 +34,9 @@ export type SeasonUpdateOut = z.output<typeof SeasonUpdateSchema>;
 // ------------ Lenient response (read) ------------
 export const SeasonReadSchema = z.looseObject({
     id: z.int(),
+    showId: z.int(),
+    index: z.int(),
     name: z.string(),
-    dwId: z.string(),
-    showId: z.string(),
     slug: z.string(),
     createdAt: z.iso.datetime().transform((s) => new Date(s)),
     updatedAt: z.iso.datetime().transform((s) => new Date(s)),
