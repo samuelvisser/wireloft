226eaa03684825f8ab23259494ffcbdddf24aaac
samuelvisser <samuvisser@gmail.com>
2026-09-06 01:25:20 +0200

    Space duration values from long unit labels

--- ui/src/utils/TimeInterval.tsx
+++ ui/src/utils/TimeInterval.tsx
@@ -231,6 +231,7 @@ export default function TimeInterval({
                             min={0}
                             step={unit === backendUnit ? step : 1}
                             value={draftParts[unit]}
+                            style={unit === 'minutes' || unit === 'seconds' ? {paddingRight: 60} : undefined}
                             onChange={onUnitChange(unit)}
                             onFocus={onFocus(unit)}
                             onMouseUp={onMouseUp(unit)}
