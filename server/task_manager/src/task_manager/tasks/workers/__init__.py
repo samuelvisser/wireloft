023b518f8f335bddaa4219573bd48d3bce7789f3
samuelvisser <samuvisser@gmail.com>
2026-09-08 12:30:46 +0200

    Fix stale identifier worker import

--- server/task_manager/src/task_manager/tasks/workers/__init__.py
+++ server/task_manager/src/task_manager/tasks/workers/__init__.py
@@ -3,10 +3,7 @@ from .debug_ep_details import debug_ep_details
 from .download_episode import download_episode
 from .download_movie import download_movie
 from .refresh_movie_extras import refresh_movie_extras
-from .download_profile_worker import (
-    download_profile_identifier_change_worker,
-    download_profile_worker,
-)
+from .download_profile_worker import download_profile_worker
 from .download_series_thumbnail import download_series_thumbnail
 from .monitor_pending_episode import monitor_pending_episode
 from .fetch_new_episodes import fetch_new_episodes
