59dedc7e2d3eada12c2d196c41a1caaac98ad288
samuvisser <samuvisser@gmail.com>
2026-09-15 13:37:47 +0200

    Locked new package versions

--- server/backend/src/backend/__init__.py
+++ server/backend/src/backend/__init__.py
@@ -1,3 +1,3 @@
 """Backend - FastAPI backend provides a local API for React to show its content."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- server/dailywire_api/src/dailywire_api/__init__.py
+++ server/dailywire_api/src/dailywire_api/__init__.py
@@ -4,4 +4,4 @@ This package communicates directly with the DailyWire services and also exposes
 a small FastAPI application mirroring the backend API structure.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
